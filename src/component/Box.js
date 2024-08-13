19e8164d383fec59983bf3b26e73af3147043115
Jeongsunga <jsa3341@gmail.com>
2024-08-13 15:21:11 +0900

    second  commit

--- src/component/Box.js
+++ src/component/Box.js
@@ -5,7 +5,7 @@ const Box = (props) => {
   return (
     <div className='box'>
         <h1>{props.title}</h1>
-        <img className='item-img' src={props.item && props.item.img}/>
+        <img className='item-img' src={props.item && props.item.img} alt='https://media.istockphoto.com/id/1131230925/vector/check-marks-red-cross-icon-simple-vector.jpg?s=612x612&w=0&k=20&c=8oNof6faYkfOn1O6CAOHwpSmAhq3IK9hY_D3icbaQps='/>
         <h2>WIN</h2>
     </div>
   )
