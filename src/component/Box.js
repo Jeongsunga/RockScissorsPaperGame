d5df39f6461dee9a338db4a77854986a37e3a021
Jeongsunga <jsa3341@gmail.com>
2024-08-13 15:27:35 +0900

    delete alt value

--- src/component/Box.js
+++ src/component/Box.js
@@ -5,7 +5,7 @@ const Box = (props) => {
   return (
     <div className='box'>
         <h1>{props.title}</h1>
-        <img className='item-img' src={props.item && props.item.img} alt='가위바위보'/>
+        <img className='item-img' src={props.item && props.item.img} alt=''/>
         <h2>WIN</h2>
     </div>
   )
